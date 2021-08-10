67f0e32b24cb452fbd738f69e4104b00949e498f
xeryto <igoshindanya@mail.ru>
2021-08-10 16:25:06 +0700

    separated data access layer and business logic

--- BusinessLogic.Test/UnitTest.cs
+++ BusinessLogic.Test/UnitTest.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using FluentAssertions;
 using System.Linq;
+using DataAccessLayer.Models;
 
 namespace BusinessLogic.Test
 {
--- BusinessLogic/DAL/IReservationRepository.cs
+++ BusinessLogic/DAL/IReservationRepository.cs
@@ -1,8 +1,6 @@
-﻿using BusinessLogic.Models;
+﻿using DataAccessLayer.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BusinessLogic.DAL
